782028f3636b44454c872a6c8b62a06c1b16476b
SilenNaihin <silen.naihin@gmail.com>
2022-01-24 14:01:44 -0500

    analysis

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,19 @@ import { useState } from "react";
 
 export default function Home() {
   const [tab, setTab] = useState(0);
-  const [tabs, setTabs] = useState([<CodingProblem />, <NextOne />]);
+  // finish this, implement dropdown
+  const [tabs, setTabs] = useState([
+    [
+      <CodingProblem />,
+      `Solving a coding problem an unconventional way. Code is un-optimized or cleaned, built the concept to prove a 
+      concept of speedup over the regular solution. This solution is O(n + n + c) with n being the number of rows and 
+      c being the number of columns. The original solution is O(n*c), nested for loops of columns and rows. Given the 
+      problem the highest amount of rows is 24, and columns is 6. If we plug in the numbers, O(24 + 24 + 6) = 54 vs 
+      O(24*6) = 144. The original is better for less than 3 columns and 10 rows, but for any more than that, my solution is more 
+      optimal (https://www.desmos.com/calculator/6cf9lijgwg).`,
+    ],
+    [<NextOne />, "Nothing here yet"],
+  ]);
 
   return (
     <>
@@ -15,11 +27,14 @@ export default function Home() {
             onClick={() => setTab(i)}
             key={i}
           >
-            {tab.type.name}
+            {tab[0].type.name}
           </div>
         ))}
       </div>
-      <div className="w-full">{tabs[tab]}</div>
+      <div className="w-full flex flex-col">
+        <p className="ml-2 my-2">{tabs[tab][1]}</p>
+        {tabs[tab][0]}
+      </div>
     </>
   );
 }
